db14678f8d3e775f6136a85d744641d9946e55e5
Demy <demym@yahoo.it>
2016-10-21 16:05:54 +0200

    201610211605

---
+++ platforms/ios/build/Bluetooth.build/Debug-iphonesimulator/Bluetooth.build/Objects-normal/i386/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/pietroturati/Desktop/PROJECTS/demy/nativescript-bluetooth-demo/Bluetooth/platforms/ios/internal/main.m \
+  /Users/pietroturati/Desktop/PROJECTS/demy/nativescript-bluetooth-demo/Bluetooth/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/pietroturati/Desktop/PROJECTS/demy/nativescript-bluetooth-demo/Bluetooth/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/pietroturati/Desktop/PROJECTS/demy/nativescript-bluetooth-demo/Bluetooth/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/pietroturati/Desktop/PROJECTS/demy/nativescript-bluetooth-demo/Bluetooth/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/pietroturati/Desktop/PROJECTS/demy/nativescript-bluetooth-demo/Bluetooth/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/pietroturati/Desktop/PROJECTS/demy/nativescript-bluetooth-demo/Bluetooth/platforms/ios/internal/TNSDebugging.h
